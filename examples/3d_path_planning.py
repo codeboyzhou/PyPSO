898c0604e2f60ff5e8b2715b69fbdea27542348a
codeboyzhou <imzhouchen@gmail.com>
2025-07-26 17:30:43 +0800

    feat(3d_path_planning): refactor terrain generation and add path plotting functionality

--- examples/3d_path_planning.py
+++ examples/3d_path_planning.py
@@ -1,34 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
-from loguru import logger
+from scipy.ndimage import gaussian_filter
+from scipy.spatial.distance import cdist
 
-from pypso.core import PyPSO, AlgorithmArguments, ProblemType
+from pypso import plot
+from pypso.core import ProblemType, PyPSO, AlgorithmArguments
 
-
-def gaussian_peak(
-    x_grid: np.ndarray,
-    y_grid: np.ndarray,
-    center_x: float,
-    center_y: float,
-    amplitude: float,
-    width: float
-) -> np.ndarray:
-    """
-    生成山峰的形状
-
-    Args:
-        x_grid (np.ndarray): X平面网格
-        y_grid (np.ndarray): Y平面网格
-        center_x (float): 山峰中心点的X坐标
-        center_y (float): 山峰中心点的Y坐标
-        amplitude (float): 山峰振幅
-        width (float): 山峰宽度
-
-    Returns:
-        山峰在Z网格的坐标
-    """
-    return amplitude * np.exp(-((x_grid - center_x) ** 2 + (y_grid - center_y) ** 2) / (2 * width ** 2))
+# 定义山峰的位置和参数
+peaks = [
+    # center_x, center_y, amplitude, width
+    (20, 20, 6, 6),
+    (20, 60, 7, 7),
+    (60, 20, 5, 8),
+    (80, 60, 5, 8),
+]
 
 
 def generate_terrain(x_grid: np.ndarray, y_grid: np.ndarray) -> np.ndarray:
@@ -42,157 +27,103 @@ def generate_terrain(x_grid: np.ndarray, y_grid: np.ndarray) -> np.ndarray:
     # 生成基础地形
     z_grid = np.zeros_like(x_grid)
 
-    # 定义山峰的位置和参数
-    peaks = [
-        # center_x, center_y, amplitude, width
-        (20, 20, 6, 6),
-        (20, 60, 7, 7),
-        (60, 20, 5, 8),
-        (80, 60, 5, 8),
-    ]
-
     # 添加山峰
     for (center_x, center_y, amplitude, width) in peaks:
-        z_grid += gaussian_peak(x_grid, y_grid, center_x, center_y, amplitude, width)
+        z_grid += amplitude * np.exp(-((x_grid - center_x) ** 2 + (y_grid - center_y) ** 2) / (2 * width ** 2))
 
     # 添加一些随机噪声和基础波动，增强山峰的真实性
     z_grid += 0.2 * np.sin(0.5 * np.sqrt(x_grid ** 2 + y_grid ** 2)) + 0.1 * np.random.normal(size=x_grid.shape)
 
     # 使用高斯滤波，保持山峰独立性的同时也保证平滑性
-    z_grid = scipy.ndimage.gaussian_filter(z_grid, sigma=3)
+    z_grid = gaussian_filter(z_grid, sigma=3)
 
     return z_grid
 
 
-def is_terrain_collision_detected(
-    point: tuple[float, float, float],
-    x_grid: np.ndarray,
-    y_grid: np.ndarray,
-    z_grid: np.ndarray
-) -> bool:
-    """
-    检测空间中某个点是否会和山体发生碰撞
+class PathPlanning3D:
 
-    Args:
-        point (tuple): 点坐标 (x, y, z)
-        x_grid (np.ndarray): X平面网格
-        y_grid (np.ndarray): Y平面网格
-        z_grid (np.ndarray): Z平面网格
+    def __init__(self):
+        # 定义路径起点和终点
+        self.start_point = (0.0, 0.0, 2.0)
+        self.destination = (80.0, 80.0, 2.0)
 
-    Returns:
-        bool: 如果点在山体内部则返回True，否则返回False
-    """
-    x, y, z = point
+        # 生成坐标网格
+        x = np.linspace(0, 100, 100)
+        y = np.linspace(0, 100, 100)
+        self.x_grid, self.y_grid = np.meshgrid(x, y)
+        self.z_grid = generate_terrain(self.x_grid, self.y_grid)
 
-    # 找到点(x, y)在网格中的最近邻接点索引
-    all_points_x_unique = x_grid[0, :]
-    all_points_y_unique = y_grid[:, 0]
-    x_neighbors_index = np.argmin(np.abs(all_points_x_unique - x))
-    y_neighbors_index = np.argmin(np.abs(all_points_y_unique - y))
+    def plot_map_with_best_path(self, best_path_points: np.ndarray) -> None:
+        """
+        绘制地形和最优路径
 
-    # 获取这些邻接点的地形高度
-    terrain_height: float = z_grid[y_neighbors_index, x_neighbors_index].item()
+        Args:
+            best_path_points (np.ndarray): 最优路径点坐标数组，形状为 (n, 3)
+        """
+        # 绘制地形
+        figure = plt.figure(figsize=(10, 8))
+        ax = figure.add_subplot(111, projection="3d")
+        surface = ax.plot_surface(self.x_grid, self.y_grid, self.z_grid, cmap="viridis", alpha=0.6)
+        figure.colorbar(surface, shrink=0.5, aspect=5)
 
-    # 如果点的Z坐标小于地形高度，则发生碰撞
-    return z < terrain_height
+        # 标记起点和终点
+        start_point_x, start_point_y, start_point_z = self.start_point[0], self.start_point[1], self.start_point[2]
+        destination_x, destination_y, destination_z = self.destination[0], self.destination[1], self.destination[2]
+        ax.scatter(start_point_x, start_point_y, start_point_z, c='green', s=100, marker='o', label='Start Point')
+        ax.scatter(destination_x, destination_y, destination_z, c='red', s=100, marker='*', label='Destination')
 
+        # 绘制路径
+        path_points = np.vstack(([self.start_point], best_path_points, [self.destination]))
+        path_x, path_y, path_z = path_points[:, 0], path_points[:, 1], path_points[:, 2]
+        ax.plot(path_x, path_y, path_z, 'b-', linewidth=5, label='Best Path')
 
-def plot_map_with_line(
-    start_point: tuple[float, float, float],
-    destination: tuple[float, float, float],
-    path_points: np.ndarray = None
-) -> None:
-    """
-    绘制基础地形和山峰，并根据给定的起点、终点、路径点数组绘制一条路径
+        # 设置坐标轴信息，elev参数控制仰角，azim参数控制方位角
+        ax.view_init(elev=30, azim=240)
+        ax.set_title("3D Path Planning")
+        ax.set_xlabel("X-axis")
+        ax.set_ylabel("Y-axis")
+        ax.set_zlabel("Height")
+        ax.legend()
 
-    Args:
-        start_point (tuple): 起点坐标 (x, y, z)
-        destination (tuple): 终点坐标 (x, y, z)
-        path_points (np.ndarray): 路径点坐标数组，形状为 (n, 3)
-    """
-    # 生成网格
-    x = np.linspace(0, 100, 100)
-    y = np.linspace(0, 100, 100)
-    x_grid, y_grid = np.meshgrid(x, y)
-    z_grid = generate_terrain(x_grid, y_grid)
+        plt.tight_layout()
+        plt.show()
 
-    # 开始绘制地形和山峰
-    figure = plt.figure(figsize=(10, 8))
-    ax = figure.add_subplot(111, projection="3d")
-    surface = ax.plot_surface(x_grid, y_grid, z_grid, cmap="viridis", alpha=0.6)
-    figure.colorbar(surface, shrink=0.5, aspect=5)
+    def compute_path_cost(self, positions: np.ndarray) -> np.ndarray:
+        """
+        适应度函数：计算路径成本
 
-    # 标记起点和终点
-    start_point_x, start_point_y, start_point_z = start_point[0], start_point[1], start_point[2]
-    destination_x, destination_y, destination_z = destination[0], destination[1], destination[2]
-    ax.scatter(start_point_x, start_point_y, start_point_z, c='green', s=100, marker='o', label='Start Point')
-    ax.scatter(destination_x, destination_y, destination_z, c='red', s=100, marker='*', label='Destination')
+        Args:
+            positions (np.ndarray): 所有粒子的位置向量
 
-    # 绘制路径
-    if path_points is not None and len(path_points) > 0:
-        # 将起点、路径点、终点连接起来
-        path_points_x, path_points_y, path_points_z = path_points[:, 0], path_points[:, 1], path_points[:, 2]
-        all_points_x = [start_point_x] + list(path_points_x) + [destination_x]
-        all_points_y = [start_point_y] + list(path_points_y) + [destination_y]
-        all_points_z = [start_point_z] + list(path_points_z) + [destination_z]
-        # 绘制完整路径
-        ax.plot(all_points_x, all_points_y, all_points_z, 'b-', linewidth=5, label='Target Path')
-
-        # 检查路径点是否与山体碰撞并标记
-        for i in range(len(path_points)):
-            point = (path_points_x[i].item(), path_points_y[i].item(), path_points_z[i].item())
-            if is_terrain_collision_detected(point, x_grid, y_grid, z_grid):
-                logger.warning(f"检测到路径采样点 {point} 存在山体碰撞风险")
-                ax.scatter(point[0], point[1], point[2], c='red', s=100, marker='x')
-
-    # 设置坐标轴信息
-    ax.set_title("3D Path Planning")
-    ax.set_xlabel("X-axis")
-    ax.set_ylabel("Y-axis")
-    ax.set_zlabel("Height")
-    ax.legend()
-    # elev参数控制仰角，azim参数控制方位角
-    ax.view_init(elev=90, azim=240)
-
-    plt.tight_layout()
-    plt.show()
-
-
-def three_dimensional_path_planning_problem(positions: np.ndarray) -> np.ndarray:
-    x, y, z = positions[:, 0], positions[:, 1], positions[:, 2]
-    return np.sqrt((x - 50) ** 2 + (y - 50) ** 2 + (z - 50) ** 2)
+        Returns:
+            路径成本大小
+        """
+        distance_matrix = cdist(positions, np.array([
+            self.start_point,
+            self.destination,
+        ]), metric='euclidean')
+        return np.sum(distance_matrix, axis=1)
 
 
 if __name__ == "__main__":
-    PyPSO(AlgorithmArguments(
+    path_planning_3d = PathPlanning3D()
+    # 初始化PSO优化器
+    pso_optimizer = PyPSO(AlgorithmArguments(
         num_particles=100,
         num_dimensions=3,
-        max_iterations=100,
-        position_bound_min=-10,
-        position_bound_max=10,
+        max_iterations=10000,
+        position_bound_min=0,
+        position_bound_max=100,
         velocity_bound_max=1,
         inertia_weight_max=2,
         inertia_weight_min=0.5,
         cognitive_coefficient=0.5,
         social_coefficient=0.5,
-        fitness_function=lambda positions: three_dimensional_path_planning_problem(positions)
-    )).start_iterating(
-        problem_type=ProblemType.MINIMIZATION_PROBLEM,
-        auto_plot_fitness_curve=True
-    )
-
-    plot_map_with_line(
-        start_point=(0, 0, 0),
-        destination=(80, 80, 2),
-        path_points=np.array([
-            [10, 0, 2],
-            [20, 0, 2],
-            [20, 20, 2],
-            [30, 0, 2],
-            [40, 0, 2],
-            [40, 40, 2],
-            [50, 50, 2],
-            [60, 60, 2],
-            [70, 70, 2],
-        ])
-    )
+        fitness_function=lambda positions: path_planning_3d.compute_path_cost(positions)
+    ))
+    # 执行算法迭代
+    best_solutions, best_fitness_values = pso_optimizer.start_iterating(ProblemType.MINIMIZATION)
+    # 绘制适应度曲线
+    plot.plot_fitness_curve(best_fitness_values)
+    # 绘制最优路径
+    path_planning_3d.plot_map_with_best_path(best_solutions)
