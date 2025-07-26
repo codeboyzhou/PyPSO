54c91441d127cb0946f0bff8284cbebb25f9e833
codeboyzhou <imzhouchen@gmail.com>
2025-07-26 17:03:12 +0800

    feat(x_square_maximization): add plotting for optimization results

--- examples/x_square_maximization_problem.py
+++ examples/x_square_maximization_problem.py
@@ -1,14 +1,19 @@
 import numpy as np
 
+from pypso import plot
 from pypso.core import PyPSO, AlgorithmArguments, ProblemType
 
 
 def x_square_maximization_problem(positions: np.ndarray) -> np.ndarray:
+    """
+    这个函数定义了一个简单的优化问题：最大化 f(x) = x^2
+    该函数接受一个位置数组，并返回每个位置的平方值作为适应度值
+    """
     return positions ** 2
 
 
 if __name__ == "__main__":
-    PyPSO(AlgorithmArguments(
+    pso_optimizer = PyPSO(AlgorithmArguments(
         num_particles=100,
         num_dimensions=1,
         max_iterations=100,
@@ -20,7 +25,6 @@ if __name__ == "__main__":
         cognitive_coefficient=0.5,
         social_coefficient=0.5,
         fitness_function=lambda positions: x_square_maximization_problem(positions)
-    )).start_iterating(
-        problem_type=ProblemType.MAXIMIZATION_PROBLEM,
-        auto_plot_fitness_curve=True
-    )
+    ))
+    best_solutions, best_fitness_values = pso_optimizer.start_iterating(ProblemType.MAXIMIZATION)
+    plot.plot_fitness_curve(best_fitness_values)
